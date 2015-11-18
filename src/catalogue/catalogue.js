e1212e35374802bc103fe8016dd8c7bb6542a6a1
Michael Langmayr <michael@Michaels-MacBook-Pro.local>
2015-11-18 23:49:53 +0000

    minor improvements

--- src/catalogue/catalogue.js
+++ src/catalogue/catalogue.js
@@ -9,33 +9,35 @@
         calculateCartTotal: '&'
       },
       controller: function() {
-        this.products = []
-        this.products.push({
+        var vm = this;
+
+        vm.products = []
+        vm.products.push({
           name: "Jeans",
           code: "J01",
           price: 32.95,
           amount: 1
         });
-        this.products.push({
+        vm.products.push({
           name: "Blouse",
           code: "B01",
           price: 24.95,
           amount: 1
         });
-        this.products.push({
+        vm.products.push({
           name: "Socks",
           code: "S01",
           price: 7.95,
           amount: 1
         });
 
-        this.buy = function(product) {
-          if(_.contains(this.cart, product)) {
-            this.cart[_.indexOf(this.cart, product)].amount++;
+        vm.buy = function(product) {
+          if(_.contains(vm.cart, product)) {
+            vm.cart[_.indexOf(vm.cart, product)].amount++;
           } else {
-            this.cart.push(product);
+            vm.cart.push(product);
           }
-          this.calculateCartTotal();
+          vm.calculateCartTotal();
         }
       },
       controllerAs: 'vm',
