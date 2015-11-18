613e6a7d028acdf5a3ceb1f521900f93d33b9667
Michael Langmayr <michael@Michaels-MacBook-Pro.local>
2015-11-18 21:04:32 +0000

    fixed bug when cart was emptied and delivery surcharge was not set to 0, activated strict mode

--- src/cartController.js
+++ src/cartController.js
@@ -1,50 +1,54 @@
-var myApp = angular.module('msBasket',[]);
+(function () {
+  'use strict';
 
-myApp.controller('CartController', ['$scope', function($scope) {
+  var myApp = angular.module('msBasket',[]);
 
-  $scope.cart = []
-  $scope.total = 0.00;
-  $scope.subtotal = 0.00;
-  $scope.delivery = 0.00;
-  $scope.discount = 0.00;
+  myApp.controller('CartController', ['$scope', function($scope) {
 
-  $scope.calculateCartTotal = function() {
-    var newTotal = 0.00;
-    _.map($scope.cart, function(item) {
-      // calculate discount on jeans, buy one pair get a second one half price
-      if(item.code === 'J01') {
-        var discountAmount = Math.floor(item.amount / 2)/2;
-        $scope.discount = discountAmount * item.price;
+    $scope.cart = []
+    $scope.total = 0.00;
+    $scope.subtotal = 0.00;
+    $scope.delivery = 0.00;
+    $scope.discount = 0.00;
+
+    $scope.calculateCartTotal = function() {
+      var newTotal = 0.00;
+      _.map($scope.cart, function(item) {
+        // calculate discount on jeans, buy one pair get a second one half price
+        if(item.code === 'J01') {
+          var discountAmount = Math.floor(item.amount / 2)/2;
+          $scope.discount = discountAmount * item.price;
+        }
+        newTotal += item.price * item.amount;
+
+      });
+
+      // consider discount on jeans, buy one pair get a second one half price
+      newTotal = newTotal - $scope.discount;
+
+      // set subtotal
+      $scope.subtotal = newTotal;
+
+      // calculate delivery surcharge
+      if(newTotal > 0 && newTotal < 50.00) {
+        $scope.delivery = 4.95;
+      } else if(newTotal >= 50.00 && newTotal < 90) {
+        $scope.delivery = 2.95;
+      } else {
+        $scope.delivery = 0.00;
       }
-      newTotal += item.price * item.amount;
 
-    });
-
-    // consider discount on jeans, buy one pair get a second one half price
-    newTotal = newTotal - $scope.discount;
-
-    // set subtotal
-    $scope.subtotal = newTotal;
-
-    // calculate delivery surcharge
-    if(newTotal < 50.00) {
-      $scope.delivery = 4.95;
-    } else if(newTotal >= 50.00 && newTotal < 90) {
-      $scope.delivery = 2.95;
-    } else if(newTotal >= 90.00) {
-      $scope.delivery = 0.00;
+      $scope.total = newTotal + $scope.delivery;
     }
 
-    $scope.total = newTotal + $scope.delivery;
-  }
-
-  $scope.removeFromCart = function(product) {
-    if(_.contains($scope.cart, product) && $scope.cart[_.indexOf($scope.cart, product)].amount > 1) {
-      $scope.cart[_.indexOf($scope.cart, product)].amount--;
-    } else if(_.contains($scope.cart, product) && $scope.cart[_.indexOf($scope.cart, product)].amount === 1) {
-        $scope.cart.splice(_.indexOf($scope.cart, product), 1);
+    $scope.removeFromCart = function(product) {
+      if(_.contains($scope.cart, product) && $scope.cart[_.indexOf($scope.cart, product)].amount > 1) {
+        $scope.cart[_.indexOf($scope.cart, product)].amount--;
+      } else if(_.contains($scope.cart, product) && $scope.cart[_.indexOf($scope.cart, product)].amount === 1) {
+          $scope.cart.splice(_.indexOf($scope.cart, product), 1);
+      }
+      $scope.calculateCartTotal();
     }
-    $scope.calculateCartTotal();
-  }
 
-}]);
+  }]);
+})();
